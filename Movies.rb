d5c55df8397f373259055799ee7bb5ef7bd7fce5
jobbink <jesse.obbink@gmail.com>
2014-08-19 10:04:23 -0700

    Test Commit Tuesday Morning

---
+++ Movies.rb
@@ -0,0 +1,12 @@
+#give a list of movies with this program
+
+movies = {:'Star Wars' => '1975',
+          :'Vampire Diaries' => '2012',
+          :'My favorite movie ever' => '1938'
+}
+
+puts movies[:'Star Wars']
+
+puts movies[:'Vampire Diaries']
+
+puts movies[:'My favorite movie ever']
